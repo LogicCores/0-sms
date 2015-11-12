1ace31f41a6cc63aa94e4af9fbd8f641b6e340da
Christoph Dorn <christoph@christophdorn.com>
2015-11-12 15:40:58 -0800

    working

--- for/twilio/0-server.api.js
+++ for/twilio/0-server.api.js
@@ -1,27 +1,62 @@
 
 exports.forLib = function (LIB) {
 
+    const TWILIO = require("twilio");
+
     var exports = {};
 
     exports.spin = function (context) {
         
         var SMS = function () {
             var self = this;
-    
-    
-            self.send = function (message) {
-    
-console.log("send SMS!!!!!!!!", message);
 
-                    if (testMode) {
+            self.send = function (messageOverrides) {
+    
+    			return LIB.Promise.promisify(function (callback) {
+
+    				if (context.config.enabled === false) {
+    					console.log("Skip sending twilio sms message due to 'enabled === false'");
+    					return callback(null);
+    				}
+
+    				LIB.assert(typeof context.config.twilio.credentials.accountSid, "string");
+    				LIB.assert(typeof context.config.twilio.credentials.authToken, "string");
+
+    				var client = TWILIO(context.config.twilio.credentials.accountSid, context.config.twilio.credentials.authToken);
+
+    				var defaultMessage = LIB._.merge({
+    				    body: "<REPLACE>",
+    				    to: "<REPLACE>",
+    				    from: "<REPLACE>"
+    				}, context.config.twilio.message || {});
+
+    				var message = LIB._.merge(defaultMessage, messageOverrides || {});
+
+                    if (message.mode === "test") {
                         message.body = "[TEST] " + (message.body || "");
                     }
-        
-                
-                
+//console.log("SMS message", message);
+    
+    				return client.messages.create(message, function (err, result) {
+    					if (err) {
+    						if (!err.stack) {
+    							var _err = new Error();
+    							for (var name in err) {
+    								_err[name] = err[name];
+    							}
+    							err = _err;
+    						}
+    						return callback(err);
+    					}
+
+//console.log("SMS send result", result);
+
+    					return callback(null);
+    				});
+    			})();
             }
         }
-    
+
         return new SMS(context);
     }
 
--- for/twilio/0-server.ccjson.js
+++ for/twilio/0-server.ccjson.js
@@ -2,8 +2,6 @@
 exports.forLib = function (LIB) {
     var ccjson = this;
 
-    const TWILIO = require("twilio");
-
     return LIB.Promise.resolve({
         forConfig: function (defaultConfig) {
 
@@ -20,57 +18,6 @@ exports.forLib = function (LIB) {
 
                         getConfig: function () {
                             return config;
-                        },
-
-                        send: function () {
-
-                            return LIB.Promise.resolve(
-                                ccjson.makeDetachedFunction(
-                            		function (messageOverrides) {
-
-                            			return LIB.Promise.promisify(function (callback) {
-
-console.log("twilio config", config);
-                            				if (config.enabled === false) {
-                            					console.log("Skip sending twilio sms message due to 'enabled === false'");
-                            					return callback(null);
-                            				}
-
-                            				LIB.assert(typeof config.twilio.credentials.accountSid, "string");
-                            				LIB.assert(typeof config.twilio.credentials.authToken, "string");
-
-                            				var client = TWILIO(config.twilio.credentials.accountSid, config.twilio.credentials.authToken);
-
-                            				var defaultMessage = LIB._.merge({
-                            				    body: "<REPLACE>",
-                            				    to: "<REPLACE>",
-                            				    from: "<REPLACE>"
-                            				}, config.twilio.message || {});
-                            
-                            				var message = LIB._.merge(defaultMessage, messageOverrides || {});
-                            
-                            console.log("SMS message", message);
-                            
-                            				return client.messages.create(message, function (err, result) {
-                            					if (err) {
-                            						if (!err.stack) {
-                            							var _err = new Error();
-                            							for (var name in err) {
-                            								_err[name] = err[name];
-                            							}
-                            							err = _err;
-                            						}
-                            						return callback(err);
-                            					}
-                            
-                            console.log("SMS send result", result);
-                            
-                            					return callback(null);
-                            				});
-                            			})();
-                            		}
-                                )
-                            );
                         }
                     });
                 }
